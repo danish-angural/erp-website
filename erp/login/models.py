760f507200cfe441be9876789d70e8f03867010f
Vamsinath123 <ayyapu.vamsinath007@gmail.com>
2021-01-15 15:05:49 +0530

    github add

--- erp/login/models.py
+++ erp/login/models.py
@@ -10,14 +10,23 @@ class Order(models.Model):
 	status=models.CharField(default='New draft', max_length=100)
 	material=models.CharField(max_length=20)
 	quantity=models.IntegerField()
-	unit=models.CharField(max_length=10,default='m')
+	unit=models.CharField(max_length=10,default='units')
 	unit_price = models.IntegerField(default=0)
 	net_price = models.IntegerField(default=0)
 	date=models.DateTimeField(auto_now=True, editable=True)
 	client=models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 	sales = models.CharField(max_length=100, default='no sales')
+	optfield1=models.CharField(blank=True, max_length=100)
+	optfield2=models.CharField(blank=True, max_length=100)
+	optfield3=models.CharField(blank=True, max_length=100)
+	optfield4=models.CharField(blank=True, max_length=100)
 
 class Details(models.Model):
 	order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
 	date = models.DateTimeField(auto_now=True)
 	status=models.CharField(default='New draft', max_length=100)
+
+class Approved_order(models.Model):
+	name = models.CharField(max_length=20)
+	unit = models.CharField(max_length=10,default='units')
+	price = models.IntegerField(default=0)
